9d2a81a850edd93a344a05ea683e7b2e92d02448
Andrey Egorov <andr06@gmail.com>
2023-10-09 02:30:22 +0300

    External database schema update for v2.0.0

--- schema.sql
+++ schema.sql
@@ -2,11 +2,18 @@
 CREATE TABLE `tg_users` (
   `id` bigint(20) NOT NULL,
   `phone` bigint(20) unsigned DEFAULT NULL,
-  `username` varchar(32) DEFAULT NULL,
   `is_bot` bool NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `phone` (`phone`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create syntax for table 'tg_usernames'
+CREATE TABLE `tg_usernames` (
+  `username` varchar(32) NOT NULL,
+  `chat_id` bigint(20) NOT NULL,
+  `order` tinyint(2) unsigned NOT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `chat_id` (`chat_id`,`order`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Create syntax for TABLE 'tg_peers'
@@ -15,6 +22,7 @@ CREATE TABLE `tg_peers` (
   `tg_peer_id` bigint(20) NOT NULL,
   `access_hash` bigint(20) NOT NULL,
   PRIMARY KEY (`tg_user_id`,`tg_peer_id`),
+  KEY `idx_tg_peer_id` (`tg_peer_id`),
   CONSTRAINT `tg_peers_ibfk_1` FOREIGN KEY (`tg_user_id`) REFERENCES `tg_users` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -35,4 +43,13 @@ CREATE TABLE `tg_auth_keys` (
   `auth_key` mediumblob NOT NULL,
   PRIMARY KEY (`tg_session_id`,`dc_id`),
   CONSTRAINT `tg_auth_keys_ibfk_1` FOREIGN KEY (`tg_session_id`) REFERENCES `tg_sessions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create syntax for TABLE 'tg_secret_chats'
+CREATE TABLE `tg_secret_chats` (
+  `tg_session_id` bigint(20) unsigned NOT NULL,
+  `secret_chat_id` int(10) NOT NULL,
+  `data` text NOT NULL,
+  PRIMARY KEY (`tg_session_id`,`secret_chat_id`),
+  CONSTRAINT `tg_secret_chats_ibfk_1` FOREIGN KEY (`tg_session_id`) REFERENCES `tg_sessions` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
